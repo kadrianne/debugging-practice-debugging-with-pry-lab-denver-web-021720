eb02698f2315928d3d5377c758120f4631c31a88
Kristine Du <kristine.a.du@gmail.com>
2020-02-15 19:20:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,10 +12,7 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.map do |crew_member|
-      binding.pry
-    puts "Hello #{crew_member}."
-  end
+  crew.map {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
